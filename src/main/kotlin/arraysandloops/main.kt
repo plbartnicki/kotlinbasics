272538a867d514934645fe6ccccad1ae7dc1bf87
bartnicki <lukasz.bartnicki.external@easy-software.com>
2022-03-20 13:07:43 +0100

    arrays, lists, lambda

--- src/main/kotlin/arraysandloops/main.kt
+++ src/main/kotlin/arraysandloops/main.kt
@@ -7,52 +7,68 @@ package arraysandloops
 
 /* Ta funkcja dla podanej liczby całkowitej zwraca ilość zer na końcu liczby */
 // f(100) -> n = 1 k=2
-fun numOfZero(arg : Int) : Int {
+fun numOfZero(arg: Int): Int {
     var k = 0
     var n = arg
-    while(n % 10 == 0) {
+    while (n % 10 == 0) {
         n /= 10    //n = n / 10
         k++
     }
     return k
 }
 
-fun digitSum(arg : Int) : Int {
+fun digitSum(arg: Int): Int {
     var sum = 0
     var n = Math.abs(arg)
-    while(n > 0) {
+    while (n > 0) {
         sum += n % 10
         n /= 10
     }
     return sum
 }
 
-fun prepareSequence(n : Int) : Array<String?> {
+fun prepareSequence(n: Int): Array<String?> {
     val array = arrayOfNulls<String>(n) //to nie jest immutable
-    for(i in 0 .. n-1) {
+    for (i in 0..n - 1) {
         array[i] = "word" + i
     }
     return array
 }
 
-fun main(args : Array<String>) {
+fun maxNumDivBy5(values: Array<Int>): Int? {
+    if (values.isEmpty()) {
+        return null
+    }
+
+    var candiate: Int? = null //kandydat na max
+    for (v in values) {
+        if (v % 5 == 0) {
+            if (candiate == null || v > candiate) {
+                candiate = v  //za candidate podstawiam nowa wieksza wartosc podzielna przez 5
+            }
+        }
+    }
+    return candiate
+}
+
+fun main(args: Array<String>) {
 
     //funkcje można zagnieżdżać
-  /*  fun f() {
-        fun g() {
+    /*  fun f() {
+          fun g() {
 
-        }
-    } */
+          }
+      } */
 
-    if(!args.isEmpty()) {
+    if (!args.isEmpty()) {
         println(args[0])
     }
 
-    for(i in 10 .. 20) {
+    for (i in 10..20) {
         print("$i ")
     }
     println()
-    for(i in 10 downTo 2 step 2)
+    for (i in 10 downTo 2 step 2)
         print("$i ")
 
     println()
@@ -66,14 +82,45 @@ fun main(args : Array<String>) {
     val array1 = arrayOf(12, 31, 14, 51, 16, 71) //arrayOf zwraca nam immutable array (nie mozna dodac wartosci)
 
     var sum = 0
-    for(v in array1) {
+    for (v in array1) {
         sum += v
     }
 
     println(sum)
 
     //println(prepareSequence(15))
-    for(v in prepareSequence(15)) {
+    for (v in prepareSequence(15)) {
         print(v + " ")
     }
+
+    println("\n" + maxNumDivBy5(arrayOf(150, -51, 24, 101, 105)))
+    println("\n" + maxNumDivBy5(arrayOf(-150, -51, -24, -5, -105)))
+    println("\n" + maxNumDivBy5(arrayOf(25)))
+    println("\n" + maxNumDivBy5(arrayOf()))
+
+    //listy
+    var immutableList = listOf<String>("abcde", "abc", "xyzv")
+    println(immutableList.toString())
+    //immutableList.add
+   // immutableList = listOf<String>("Word", "Word2", "Word3")
+
+    val mutableList = mutableListOf<Double>(1.0, -9.0, -0.01)
+    mutableList.add(10.01)
+    //mutableList.set(1, 0.01)
+
+    println()
+    //chcemy przygotowac liste z wartosciami z listy immutableList ale od konca i o dlugosci >= 4
+    //"abcde", "abc", "xyzv" -> "edcba", "vzyx"
+
+    val resultlist = immutableList.filter { it -> it.length >= 4 }.map { it.reversed() }
+    println(resultlist)
+
+    val numbers = IntArray(10) {3*it}
+    numbers.forEach { it -> println(it)  }
+
+    fun sumValuesLess0(nums: List<Double>):Double {
+        return nums.filter{it -> it < 0}.reduce { acc, d -> acc + d}
+    }
+
+    println(sumValuesLess0(mutableList))
 }
